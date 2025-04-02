865e7e50d21c06054ba6a2199fb00178f85662f0
MullBang <hihellow18@gmail.com>
2025-04-02 15:49:01 +0900

    5-1

---
+++ Ch05/Assets/2 Scripts/ArrowController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+    public float moveStep = 0.07f;
+    public float r1 = 0.4f;
+    public float r2 = 0.9f;
+    GameObject player;
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        this.player = GameObject.Find("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -moveStep, 0);
+
+        if (transform.position.y < -7f)
+        {
+            Destroy(gameObject);
+        }
+
+        Vector2 p1 = transform.position;
+        Vector2 p2 = this.player.transform.position;
+        Vector2 dir = p1 - p2;
+        float distance = dir.magnitude;
+
+        if(distance < r1 + r2)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
